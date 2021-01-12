a402ea67a647abb42b3df091aac10290b7703876
mmkarakaya <karakaya.mehmet.melih@gmail.com>
2021-01-12 21:53:06 +0300

    Main Update

--- BankWallet2/src/sample/Main.java
+++ BankWallet2/src/sample/Main.java
@@ -18,18 +18,38 @@ public class Main extends Application {
     private static final HashMap<String,Scene> scenes = new HashMap<>();
 
     private static final DTuple[] dTuples = new DTuple[] {
-            new DTuple("loginPage.fxml", "Login"),
-            new DTuple("signUpPage.fxml", "Sign Up"),
-            new DTuple("forgotPasswordPage.fxml", "Forgot Password"),
-            new DTuple("homePage.fxml", "Home"),
-            new DTuple("stockPage.fxml", "Stock Market"),
-            new DTuple("expensePage.fxml", "Expenses and Gross"),
-            new DTuple("billPage.fxml", "Automate Your Bills"),
-            new DTuple("accountPage.fxml", "Accounts"),
-            new DTuple("profilePage.fxml", "Profile"),
-            new DTuple("stock 4.fxml", "Stock News"),
-            new DTuple("stock 2.fxml", "Stock Extra"),
-            new DTuple("stock 3.fxml", "Stock 3")
+            new DTuple("loginPage.fxml", "Login"), // 0
+            new DTuple("signUpPage.fxml", "Sign Up"), // 1
+            new DTuple("forgotPasswordPage.fxml", "Forgot Password"), // 2
+            new DTuple("homePage.fxml", "Home"), // 3
+            new DTuple("stockPage.fxml", "Stock Market"), // 4
+            new DTuple("expensePage.fxml", "Expenses and Gross"), // 5
+            new DTuple("billPage.fxml", "Automate Your Bills"), // 6
+            new DTuple("account.fxml", "Accounts"), // 7
+            new DTuple("profilePage.fxml", "Profile"), // 8
+            new DTuple("stock 4.fxml", "Stock"), // 9
+            new DTuple("stock 2.fxml", "Stock"), // 10
+            new DTuple("stock 3.fxml", "Stock"), // 11
+            new DTuple("expensePage2.fxml", "Expense Page"), // 12
+            new DTuple("expensePage3.fxml", "Expense Page"), // 13
+            new DTuple("LanguagePage.fxml", "Language Page"), // 14
+            new DTuple("TimeSettingPage.fxml", "Time Setting Page"), // 15
+            new DTuple("account2.fxml", "Account Page"), // 16
+            new DTuple("account3.fxml", "Account Page"), // 17
+            new DTuple("billPage2.fxml", "Bill Page"), // 18
+            new DTuple("billPage3.fxml", "Bill Page"), // 19
+            new DTuple("billPage4.fxml", "Bill Page"), // 20
+            new DTuple("PopupDecont.fxml", "Popup Decont Page"), // 21
+            new DTuple("SendMoney1.fxml", "Send Money"), // 22
+            new DTuple("SendMoney1b.fxml", "Send Money"), // 23
+            new DTuple("SendMoney2.fxml", "Send Money"), // 24
+            new DTuple("sendMoney3.fxml", "Send Money"), // 25
+            new DTuple("SendMoney4.fxml", "Send Money"), /// 26
+            new DTuple("SecurityPreferencesPage.fxml", "Security Preferences"), // 27
+            new DTuple("PopUpBackUpData.fxml", "Backup Data"), // 28
+            new DTuple("changeEmailPage.fxml", "Change Email"), // 29
+            new DTuple("changePasswordPage.fxml", "Change Password"), // 30
+            new DTuple("MenuDesignPage.fxml", "Menu Design"), // 31
     };
 
     private static final DTuple[] itPages = new DTuple[] {
@@ -41,9 +61,23 @@ public class Main extends Application {
             dTuples[8],
     };
 
+    private static final LinkedList<DTuple> lastPages = new LinkedList<>();
+
+    public static void billPageGotoLastPage() {
+        if (lastPages.size() >= 2) {
+            gotoPage(lastPages.get(lastPages.size() - 2));
+        } else {
+            gotoPage(lastPages.getLast());
+        }
+    }
+
+    public static void billPageAddLastPage(DTuple d) {
+        lastPages.add(d);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Font.loadFont(Main.class.getResource("PlayfairDisplay-Regular.ttf").toExternalForm(), 10);
+        Font.loadFont(Main.class.getResource("dejavu/ttf/DejaVuSerif.ttf").toExternalForm(), 10);
         stage = primaryStage;
 
         for (DTuple dTuple : dTuples) {
